06d7267b76a2021e704f0f59f211e9d2ad8909cb
TrejGun <trejgun@gmail.com>
2022-03-09 14:15:35 +0800

    feat: @typescript-eslint/no-misused-promises

--- rules/typescript.js
+++ rules/typescript.js
@@ -13,6 +13,7 @@ module.exports = {
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-misused-promises": "warn",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
