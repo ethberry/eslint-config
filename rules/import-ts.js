fff521400270c2fc75610bf99570b3831b6d9384
TrejGun <trejgun@gmail.com>
2022-03-10 22:42:52 +0800

    fix: mocha test folder

--- rules/import-ts.js
+++ rules/import-ts.js
@@ -7,7 +7,7 @@ module.exports = {
       "error",
       {
         devDependencies: [
-          "**/tests/**/*.{ts,tsx,js,jsx}",
+          "**/test/**/*.{ts,tsx,js,jsx}",
           "**/*.test.{ts,tsx,js,jsx}",
           "**/*.spec.{ts,tsx,js,jsx}",
           "**/*.stories.{ts,tsx,js,jsx}",
