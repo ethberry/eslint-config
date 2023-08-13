b7a86ed65b7aea1f6d6d9ef87df74e2434cffe13
TrejGun <trejgun@gmail.com>
2023-08-14 07:24:00 +0800

    chore: update eslint-plugin-testing-library

--- tests/testing-library.js
+++ tests/testing-library.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: ["testing-library"],
   rules: {
     "testing-library/await-async-query": "error",
-    "testing-library/no-await-sync-query": "error",
+    "testing-library/no-await-sync-queries": "error",
     "testing-library/no-debugging-utils": "warn",
     "testing-library/no-dom-import": "off",
   },
