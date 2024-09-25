fdfadee190f8105643839c25fcbaa38e16131f56
TrejGun <trejgun@gmail.com>
2024-09-25 22:04:12 +0400

    fix: node rules

--- rules/node.mjs
+++ rules/node.mjs
@@ -1,10 +1,21 @@
 import nodePlugin from "eslint-plugin-n";
 
 export default [
-  nodePlugin.configs["flat/recommended-module"],
+  // base
   {
+    files: ["**/*.{js,mjs,ts,mts}"],
+    ...nodePlugin.configs["flat/recommended-module"],
+  },
+
+  // overrides
+  {
+    files: ["**/*.{js,mjs,ts,mts}"],
     rules: {
       "n/exports-style": ["error", "exports"],
+    },
+  },
+  {
+    rules: {
       // https://github.com/eslint-community/eslint-plugin-n/issues/314
       "n/no-missing-import": "off",
     },
