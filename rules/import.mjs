28ff9c6399e456140c5e02a905cfd312624b4356
TrejGun <trejgun@gmail.com>
2025-02-07 14:41:36 +0400

    fix: add grouping rules

--- rules/import.mjs
+++ rules/import.mjs
@@ -2,5 +2,23 @@ import importPlugin from "eslint-plugin-import";
 
 export default [
   importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript
+  importPlugin.flatConfigs.typescript,
+  {
+    rules: {
+      'import/order': [
+        "error",
+        {
+          "pathGroups": [
+            {
+              "pattern": "@{ethberry,framework}/**",
+              "group": "external",
+              "position": "after"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          'newlines-between': 'always',
+        },
+      ],
+    }
+  }
 ]
